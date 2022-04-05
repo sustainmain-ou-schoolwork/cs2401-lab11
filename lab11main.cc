491e4444c4f1ccf2bebceaa07bb1bee258055bbb
Josh Marusek <jm553020@ohio.edu>
2022-04-05 17:17:10 -0400

    finish part 2

--- lab11main.cc
+++ lab11main.cc
@@ -2,9 +2,15 @@
 
 using namespace std;
 
+
 int counting(int n);
 
+void print_stars(int n);
+void see_stars(int count, int stop);
+
+
 int main() {
+    // main for part 1
     int x;
     for (int j = 0; j < 3; ++j) {
         cout << "Enter a number: ";
@@ -13,7 +19,11 @@ int main() {
         counting(x);
     }
 
-    // main for part 2 will go here
+    // main for part 2
+    int n;
+    cout << "Enter a number between 1 and 30 to see the stars\n";
+    cin >> n;
+    see_stars(1, n);
 
     return 0;
 }
@@ -30,5 +40,30 @@ int counting(int n) {
             cout << n << '\n';
         }
         return 0;
+    }
+}
+
+void print_stars(int n) {
+    if (n <= 0) {
+        // base case
+        cout << '\n';
+    }
+    else {
+        // variant expression
+        cout << '*';
+        print_stars(n - 1);
+    }
+}
+
+void see_stars(int count, int stop) {
+    if (count <= 0 || count > stop) {
+        // base case
+        return;
+    }
+    else {
+        // variant expression
+        print_stars(count);
+        see_stars(count + 1, stop);
+        print_stars(count);
     }
 }
